dbedc5495a92b13628de6a956eec315e97173b21
Antonios Zachos <55564849+antoniozah@users.noreply.github.com>
2021-10-23 23:33:00 +0300

    fix craco config

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,7 @@
-import tailwindcss from 'tailwindcss';
-export const style = {
-    postcss: {
-        plugins: [tailwindcss('./tailwind.config.js'), require('autoprefixer')],
+module.exports = {
+    style: {
+        postcss: {
+            plugins: [require('tailwindcss'), require('autoprefixer')],
+        },
     },
 };
